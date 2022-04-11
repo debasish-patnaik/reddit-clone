22f18407fde6db20168d3d6aac832936c262a258
Debasish Patnaik <imdebasishpatnaik@gmail.com>
2022-04-11 11:03:43 +0530

    feat: user details of a post can be queried

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -43,22 +43,51 @@ export class PostResolver {
   @Query(() => PaginatedPosts)
   async posts(
     @Arg('limit', () => Int) limit: number,
-    @Arg('cursor', () => String, { nullable: true }) cursor: string
+    @Arg('cursor', () => String, { nullable: true }) cursor: string | null
   ): Promise<PaginatedPosts> {
     const realLimit = Math.min(50, limit);
     const realLimitPlusOne = realLimit + 1;
 
-    const qb = dataSource
-      .getRepository(Post)
-      .createQueryBuilder('posts')
-      .orderBy('"createdAt"', 'DESC')
-      .take(realLimitPlusOne);
+    const replacements: any[] = [realLimitPlusOne];
 
     if (cursor) {
-      qb.where('"createdAt" < :cursor', { cursor: new Date(parseInt(cursor)) });
+      replacements.push(new Date(parseInt(cursor)));
     }
 
-    const posts = await qb.getMany();
+    const posts = await dataSource.query(
+      `
+    select p.*,
+    json_build_object(
+      'id', u.id,
+      'username', u.username,
+      'email', u.email,
+      'createdAt', u."createdAt",
+      'updatedAt', u."updatedAt"
+      ) creator
+    from post p
+    inner join public.user u on u.id = p."creatorId"
+    ${cursor ? `where p."createdAt" < $2` : ''}
+    order by p."createdAt" DESC
+    limit $1
+    `,
+      replacements
+    );
+
+    // const qb = dataSource
+    //   .getRepository(Post)
+    //   .createQueryBuilder('posts')
+    //   .innerJoinAndSelect('posts.creator', 'u', 'u.id = posts."creatorId"')
+    //   .orderBy('posts."createdAt"', 'DESC')
+    //   .take(realLimitPlusOne);
+
+    // if (cursor) {
+    //   qb.where('posts."createdAt" < :cursor', {
+    //     cursor: new Date(parseInt(cursor)),
+    //   });
+    // }
+
+    // const posts = await qb.getMany();
+    console.log('posts', posts);
 
     return {
       posts: posts.slice(0, realLimit),

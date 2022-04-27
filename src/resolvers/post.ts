2d69d5c5262196c2660f52dcbf63cf8678a5ca02
Debasish Patnaik <imdebasishpatnaik@gmail.com>
2022-04-27 15:40:04 +0530

    feat: handles the case when user updoots or downdoots a previously voted post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -14,6 +14,7 @@ import {
 } from 'type-graphql';
 import { dataSource } from '../dataSource';
 import { Post } from '../entities/Post';
+import { Updoot } from '../entities/Updoot';
 import { isAuth } from '../middleware/isAuth';
 import { MyContext } from '../types';
 
@@ -50,26 +51,50 @@ export class PostResolver {
     const isUpdoot = value !== -1;
     const realValue = isUpdoot ? 1 : -1;
     const { userId } = req.session;
-    // await Updoot.insert({
-    //   userId,
-    //   postId,
-    //   value: realValue,
-    // });
 
-    await dataSource.query(
-      `
-    START TRANSACTION;
-    
-    insert into updoot ("userId", "postId", "value")
-    values (${userId}, ${postId}, ${realValue});
-    
-    update post
-    set points = points + ${realValue}
-    where id = ${postId};
-    
-    COMMIT;
-    `
-    );
+    const updoot = await Updoot.findOneBy({ postId, userId });
+
+    // the user has voted on the post before
+    // and they are changing their vote
+    if (updoot && updoot.value !== realValue) {
+      await dataSource.transaction(async (tm) => {
+        await tm.query(
+          `
+        update updoot
+        set value = $1
+        where "postId" = $2 and "userId" = $3
+        `,
+          [realValue, postId, userId]
+        );
+
+        await tm.query(
+          `
+        update post
+        set points = points + $1
+        where id = $2`,
+          [2 * realValue, postId]
+        );
+      });
+    } else if (!updoot) {
+      //user has never voted before
+      await dataSource.transaction(async (tm) => {
+        await tm.query(
+          `
+        insert into updoot ("userId", "postId", "value")
+        values ($1, $2, $3)
+    `,
+          [userId, postId, realValue]
+        );
+
+        await tm.query(
+          `
+        update post
+        set points = points + $1
+        where id = $2`,
+          [realValue, postId]
+        );
+      });
+    }
 
     return true;
   }
